ab88ed5db7419d5f7d971f014bd1af0928cec02f
Rafał Miczek <miczek.r@gmail.com>
2023-01-19 23:24:17 +0100

    Notification Controller tests.

---
+++ UnitTests/MockData/NotificationMockData.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTOs.Notification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.MockData
+{
+    public class NotificationMockData
+    {
+        public static List<NotificationBaseDTO> GetNotifications()
+        {
+            return new List<NotificationBaseDTO>
+            {
+                new NotificationBaseDTO
+                {
+                    Id = "testNotificationId1",
+                    CreatedTime = DateTime.Now,
+                    Status = Core.Enums.NotificationStatus.New,
+                    Title = "Test Notification 1",
+                    Text = "This is Test Notification 1"
+                },
+                 new NotificationBaseDTO
+                {
+                    Id = "testNotificationId2",
+                    CreatedTime = DateTime.Now,
+                    Status = Core.Enums.NotificationStatus.New,
+                    Title = "Test Notification 2",
+                    Text = "This is Test Notification 2"
+                }
+            };
+        }
+    }
+}
