94cef1fbd4737f272cada8ed9dd9ac00b1e8cd27
Rafał Miczek <miczek.r@gmail.com>
2023-01-19 23:23:48 +0100

    Device Controller tests.

---
+++ UnitTests/MockData/DeviceMockData.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.Device;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.MockData
+{
+    public class DeviceMockData
+    {
+        public static List<DeviceBaseDTO> GetDevices()
+        {
+            return new List<DeviceBaseDTO>
+            {
+                new DeviceBaseDTO
+                {
+                    Name = "First Test Device",
+                    Description = "This is first test device",
+                    UUID = "1234567890",
+                }
+            };
+        }
+    }
+}
