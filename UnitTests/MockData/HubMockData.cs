f47c4fbb3338594f5457ba4880f3ae1e7356ce57
Rafał Miczek <miczek.r@gmail.com>
2023-01-22 13:35:34 +0100

    Added hub repository tests.

--- UnitTests/MockData/HubMockData.cs
+++ UnitTests/MockData/HubMockData.cs
@@ -1,6 +1,7 @@
 ﻿using Application.DTOs.Device;
 using Application.DTOs.Hub;
 using Application.DTOs.User;
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -63,6 +64,80 @@ namespace UnitTests.MockData
             {
                 MacAddress = "testMac1",
                 Password = "testPassword1"
+            };
+        }
+
+        public static Hub GetHubEntity()
+        {
+            return new Hub
+            {
+                Id = 1,
+                Name = "Test Hub 1",
+                Description = "This is Test Hub 1",
+                MacAddress = "testMac1",
+                Password = "testPassword1",
+                Devices = DeviceMockData.GetDeviceEntities(),
+                Users = UserMockData.GetUserEntities()
+            };
+        }
+
+        public static List<Hub> GetHubEntities()
+        {
+            return new List<Hub>
+            {
+            new Hub
+            {
+                Name = "Test Hub 1",
+                Description = "This is Test Hub 1",
+                MacAddress = "testMac1",
+                Password = "testPassword1",
+                Devices = DeviceMockData.GetDeviceEntities(),
+                Users = UserMockData.GetUserEntities()
+            },
+                new Hub
+            {
+                Name = "Test Hub 2",
+                Description = "This is Test Hub 2",
+                MacAddress = "testMac2",
+                Password = "testPassword2",
+                Devices = DeviceMockData.GetDeviceEntities(),
+                Users = UserMockData.GetUserEntities()
+            },
+                new Hub
+            {
+                Name = "Test Hub 3",
+                Description = "This is Test Hub 3",
+                MacAddress = "testMac3",
+                Password = "testPassword3",
+                Devices = DeviceMockData.GetDeviceEntities(),
+                Users = UserMockData.GetUserEntities()
+            }
+            };
+        }
+
+        public static Hub NewHubEntity()
+        {
+            return new Hub
+            {
+                Name = "Test Hub 4",
+                Description = "This is Test Hub 4",
+                MacAddress = "testMac4",
+                Password = "testPassword4",
+                Devices = DeviceMockData.GetDeviceEntities(),
+                Users = UserMockData.GetUserEntities()
+            };
+        }
+
+        public static Hub EmptyHubEntity()
+        {
+            return new Hub
+            {
+                Name = "Empty Hub",
+                Description = "This is Empty Hub",
+                MacAddress = "emptyHub1",
+                Password = "emptyHubPassword1",
+                Devices = new List<Device>(),
+                Users = new List<User>()
             };
         }
     }
