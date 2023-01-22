18c22797d613e037a4e8fabd680eb432cc411acc
Rafał Miczek <miczek.r@gmail.com>
2023-01-22 13:37:54 +0100

    Added user repository tests. Removed sensorcondition.

---
+++ Core/Specifications/UserSpecification.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Core.Specifications.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class UserSpecification: BaseSpecification<User>
+    {
+        public UserSpecification(Expression<Func<User,bool>> criteria): base(criteria)
+        {
+
+        }
+    }
+}
